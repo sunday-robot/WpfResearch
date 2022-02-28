d15c2351f001fac5947a11e90e828554f6da4646
sunday-robot <sgx03150@nifty.com>
2022-02-28 15:18:23 +0900

    a

---
+++ ListViewResearch2/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListViewResearch2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataContextClass DataContextClass = new DataContextClass();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class DataContextClass
+    {
+        public ObservableCollection<ListViewItemData> ListViewItemsSource { get; private set; }
+
+        public DataContextClass()
+        {
+            this.ListViewItemsSource = new ObservableCollection<ListViewItemData>();
+            this.ListViewItemsSource.Add(new ListViewItemData { Column1 = "A1", Column2 = "B1" });
+            this.ListViewItemsSource.Add(new ListViewItemData { Column1 = "A2", Column2 = "B2" });
+            this.ListViewItemsSource.Add(new ListViewItemData { Column1 = "A3", Column2 = "B3" });
+        }
+    }
+
+    public struct ListViewItemData
+    {
+        public string Column1 { get; set; }
+        public string Column2 { get; set; }
+    }
+}
---
+++ ScrollBarResearch/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScrollBarResearch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListView1.Items.Add("A001");
+            ListView1.Items.Add("A002");
+            ListView1.Items.Add("A003");
+            ListView1.Items.Add("A004");
+            ListView1.Items.Add("A005");
+            ListView1.Items.Add("A006");
+        }
+    }
+}
